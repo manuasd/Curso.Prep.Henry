4fd1a7d0826c4a91e16afb0ad83b82a9a21049fb
manuasd <60719602+manuasd@users.noreply.github.com>
2021-12-25 19:26:18 -0300

    testing

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,8 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  var palabra = str
+  return palabra
 }
 
 function suma(x, y) {
