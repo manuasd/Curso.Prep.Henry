525b5df6089288df01f2e9b038780e6858049b3a
manuasd <60719602+manuasd@users.noreply.github.com>
2022-01-02 13:47:23 -0300

    testing

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -48,6 +48,7 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // Devuelve el objeto
   // Tu código:
   delete objeto[unaPropiedad];
+  return objeto;
 }
 
 function nuevoUsuario(nombre, email, password) {
