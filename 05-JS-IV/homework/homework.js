5dd898a9d3ebe8b241e6890279807a83c0e32b99
manuasd <60719602+manuasd@users.noreply.github.com>
2022-01-02 13:36:33 -0300

    testing

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -38,9 +38,7 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-  var objetoMisterioso = {
-    numeroMisterioso: 5
-  }
+  
   return objetoMisterioso.numeroMisterioso * 5
 }
 
