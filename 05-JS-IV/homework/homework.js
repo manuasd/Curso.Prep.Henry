26b9cde59e880e3ebfe860cc600b469933dd69fd
manuasd <60719602+manuasd@users.noreply.github.com>
2022-01-02 13:48:15 -0300

    testing

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -67,7 +67,7 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
-  usuario[email] !== null;
+  return usuario.email !== null;
 }
 
 
