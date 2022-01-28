ba81377b0f4b8951c0e119213964faadb2ff0c69
manuasd <60719602+manuasd@users.noreply.github.com>
2022-01-27 21:22:51 -0300

    testing

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,26 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var mayus = '';
+  mayus = nombre[0].toUpperCase();
+  for(var i = 1; i <= nombre.length - 1; i++)
+  mayus += nombre[i];
+  return  mayus
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  function cb(n1,n2){
+    return n1 + n2
+  }
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +31,17 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  numeros.forEach(function(elemento,indice){
+    elemento += elemento;
+  })
+
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb)
 }
 
 function map(array, cb) {
@@ -35,12 +49,14 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  nArray = array.map(cb())
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let nArray = array.filter(function(array){return array[0] === 'a'})
 }
 
 // No modificar nada debajo de esta línea
